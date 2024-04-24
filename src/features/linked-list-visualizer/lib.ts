14d29badcf16bf9eafa36adfc0e2f75fded222a1
nikolay <n.smolov@yandex.ru>
2024-04-24 16:43:45 +0300

    prepare all linked list methods

--- src/features/linked-list-visualizer/lib.ts
+++ src/features/linked-list-visualizer/lib.ts
@@ -21,38 +21,23 @@ export class LinkedList extends FrameMaker<RenderNode> {
 	private _size;
 	private _maxSize;
 
-	constructor(maxSize: number, values?: (string | number)[]) {
+	constructor(maxSize: number = Infinity, values?: (string | number)[]) {
 		super();
 
 		this._maxSize = maxSize;
 		this._size = 0;
 
-		if (!values || values.length == 0) return this;
+		if (!values || values.length === 0) return this;
 
 		this.head = new RenderNode(String(values[0]));
+		let curNode = this.head;
 		this._size += 1;
 
-		let curNode = this.head;
 		for (let i = 1; i < values.length && i < this._maxSize; i += 1) {
 			curNode.next = new RenderNode(String(values[i]));
 			curNode = curNode.next;
 			this._size += 1;
 		}
-	}
-
-	protected _frame(): void {
-		const array: RenderNode[] = [];
-
-		if (this.head) {
-			let curNode = this.head;
-
-			for (let i = 0; i < this._size; i += 1) {
-				array.push(curNode);
-				if (!curNode.next) break;
-				curNode = curNode.next;
-			}
-		}
-		this.onFrame(array);
 	}
 
 	get size() {
@@ -61,5 +46,45 @@ export class LinkedList extends FrameMaker<RenderNode> {
 
 	get maxSize() {
 		return this._maxSize;
+	}
+
+	protected _frame(): void {
+		const array: RenderNode[] = [];
+
+		let curNode = this.head;
+
+		while (curNode) {
+			array.push({ ...curNode });
+			if (!curNode.next) break;
+			curNode = curNode.next;
+		}
+
+		this.onFrame(array);
+	}
+
+	private _removeByIdx(idx: number) {}
+
+	private _pop() {}
+
+	public append(value: string) {}
+
+	public pop(idx?: number) {
+		if (!idx) return this._pop();
+
+		if (idx < 0 || idx >= this._size) {
+			throw Error('index out of range');
+		}
+
+		return this._removeByIdx(idx);
+	}
+
+	public unshift(value: string) {}
+
+	public shift() {}
+
+	public insert(idx: number, value: string) {
+		if (idx < 0 || idx >= this._size) {
+			throw Error('index out of range');
+		}
 	}
 }
